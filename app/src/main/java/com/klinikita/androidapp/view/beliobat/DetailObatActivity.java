710809a2761fd687e7e74613deca4731ed43d2a5
DESKTOP-17QH47K\Febriyan A. Fatma <febriyanadifatma13021996@gmail.com>
2017-11-23 05:46:13 +0700

    fix

--- app/src/main/java/com/klinikita/androidapp/view/beliobat/DetailObatActivity.java
+++ app/src/main/java/com/klinikita/androidapp/view/beliobat/DetailObatActivity.java
@@ -1,9 +1,7 @@
 package com.klinikita.androidapp.view.beliobat;
 
-import android.content.Intent;
-
 import android.app.ProgressDialog;
-
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.Bitmap;
 import android.net.Uri;
@@ -32,14 +30,12 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.IOException;
+import java.text.BreakIterator;
 
 import okhttp3.ResponseBody;
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
-
-import java.io.IOException;
-import java.text.BreakIterator;
 
 public class DetailObatActivity extends AppCompatActivity {
     //logt
@@ -49,11 +45,13 @@ public class DetailObatActivity extends AppCompatActivity {
     Boolean isFavorit;
     SharedPreferences pref;
     FloatingActionButton fab;
+    TextView hargaobat;
+    int quantity = 0;
+    String lat = "";
+    String lng = "";
     private Button btnBeli;
     private String dataId;
     private String dataHarga;
-    TextView hargaobat;
-    int quantity = 0;
     private TextView Obat;
     private TextView HargaObat;
     private TextView Deskripsi;
@@ -64,7 +62,6 @@ public class DetailObatActivity extends AppCompatActivity {
     private TextView totalharga;
     private TextView bayartunai;
     private Button btnPesan;
-
     private TextView tvPlaceAPI;
     // konstanta untuk mendeteksi hasil balikan dari place picker
     private int PLACE_PICKER_REQUEST = 1;
@@ -89,6 +86,15 @@ public class DetailObatActivity extends AppCompatActivity {
         dataGambar = getIntent().getExtras().getString(Konstanta.DATA_GAMBAR);
         dataHarga = getIntent().getExtras().getString(Konstanta.DATA_HARGA);
 
+        initView();
+
+        btnBeli.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                kirimNotif();
+            }
+        });
+
         hargaobat.setText(dataHarga);
         perkiraanharga.setText(dataHarga);
         biayaantar.setText(dataHarga);
@@ -112,94 +118,10 @@ public class DetailObatActivity extends AppCompatActivity {
                 }
 
 
-
-//
-//        //logd untuk menampilkan di logcat
-//        Log.d(TAG, "onCreate: " + dataNama + dataGambar + dataDeskripsi + dataAlamat);
-//
-//        //ambil data dari sharedpreference
-//        pref = getSharedPreferences(Konstanta.SETTING, MODE_PRIVATE);
-//        isFavorit = pref.getBoolean(Konstanta.TAG_PREF+dataNama, false);
-//
-//
-//        fab = (FloatingActionButton)findViewById(R.id.fab);
-//        cekFavorit(isFavorit);
-//        fab.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                //simpan ke favorit jika pref false
-//
-//                if(isFavorit){
-//                    //jika love penuh
-//                    //hapus dari sqlite
-//                    long id = database.delete(dataNama);
-//
-//                    if (id<=0){
-//                        Snackbar.make(view, "Favorit gagal dihapus dari database", Snackbar.LENGTH_LONG)
-//                                .setAction("Action", null).show();
-//                    } else {
-//                        Snackbar.make(view, "Favorit dihapus dari database", Snackbar.LENGTH_LONG)
-//                                .setAction("Action", null).show();
-//
-//                        //bikin false
-//                        SharedPreferences.Editor editor = pref.edit();
-//                        editor.putBoolean(Konstanta.TAG_PREF+dataNama, false);
-//                        editor.commit();
-//                        isFavorit = false;
-//                    }
-//
-//                } else {
-//                    //jika love kosong
-//                    //simpan ke sqlite
-//                    long id = database.insertData(dataNama,dataGambar, dataAlamat, dataDeskripsi, "12.232", "3.212");
-//
-//                    Log.d(TAG, "id kembalian: "+id);
-//                    if (id<=0){
-//                        Snackbar.make(view, "Favorit gagal ditambahkan ke database", Snackbar.LENGTH_LONG)
-//                                .setAction("Action", null).show();
-//                    } else {
-//                        Snackbar.make(view, "Favorit ditambahkan ke database", Snackbar.LENGTH_LONG)
-//                                .setAction("Action", null).show();
-//
-//                        //bikin true
-//                        SharedPreferences.Editor editor = pref.edit();
-//                        editor.putBoolean(Konstanta.TAG_PREF+dataNama, true);
-//                        editor.commit();
-//                        isFavorit = true;
-//                    }
-//
-//                }
-//
-//                cekFavorit(isFavorit);
-//
-//            }
-//        });
-//
-//
-//
-//        TextView tvAlamat = (TextView) findViewById(R.id.tv_detail_alamat);
-//        TextView tvDeskripsi = (TextView) findViewById(R.id.tv_detail_deskripsi);
-//        ImageView ivGambar = (ImageView) findViewById(R.id.iv_detail_gambar);
-//
-//        tvAlamat.setText(dataAlamat);
-//        tvDeskripsi.setText(dataDeskripsi);
-//        Glide.with(DetailObatActivity.this).load("http://52.187.117.60/wisata_semarang/img/wisata/"+dataGambar).into(ivGambar);
-//
-//        getSupportActionBar().setTitle(dataNama);
-
-                initView();
-
-                btnBeli.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        kirimNotif();
-                    }
-                });
             }
         });
     }
-    String lat = "";
-    String lng = "";
+
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent imageReturnedIntent) {
         super.onActivityResult(requestCode, resultCode, imageReturnedIntent);
@@ -222,7 +144,7 @@ public class DetailObatActivity extends AppCompatActivity {
             String information = String.format("%s", place.getName());
             lat = String.valueOf(place.getLatLng().latitude);
             lng = String.valueOf(place.getLatLng().longitude);
-            BreakIterator statusMaps=null;
+            BreakIterator statusMaps = null;
             statusMaps.setText(information);
         }
 
@@ -230,8 +152,8 @@ public class DetailObatActivity extends AppCompatActivity {
         hargaobat.setText("" + Integer.parseInt(dataHarga) * quantity);
         perkiraanharga.setText("" + Integer.parseInt(dataHarga) * quantity);
         biayaantar.setText("" + (5000));
-        totalharga.setText("" + ((Integer.parseInt(dataHarga) * quantity)+5000));
-        bayartunai.setText("" + ((Integer.parseInt(dataHarga) * quantity)+5000));
+        totalharga.setText("" + ((Integer.parseInt(dataHarga) * quantity) + 5000));
+        bayartunai.setText("" + ((Integer.parseInt(dataHarga) * quantity) + 5000));
 
         display(quantity);
     }
@@ -243,21 +165,21 @@ public class DetailObatActivity extends AppCompatActivity {
         progress.show();
 
         SharedPreferences pref = getSharedPreferences(Konstanta.SETTING, MODE_PRIVATE);
-        String nomertelp = pref.getString(Konstanta.NO_TELP,"08");
+        String nomertelp = pref.getString(Konstanta.NO_TELP, "08");
 //        Toast.makeText(this, ""+nomertelp, Toast.LENGTH_SHORT).show();
 
         ApiService api = Client.getApiService(Config.BASE_URL_SMSNOTIF);
-        Call<ResponseBody> call = api.kirimNotif(nomertelp, "Terima kasih sudah melakukan pemesanan Obat di Klinik Kita. Silahkan lakukan pembayaran segera sejumlah "+ biayaantar.getText().toString() +"ke Rek. 324424234 a.n Kalif Ardy sebelum pukul 16.00");
+        Call<ResponseBody> call = api.kirimNotif(nomertelp, "Terima kasih sudah melakukan pemesanan Obat di Klinik Kita. Silahkan lakukan pembayaran segera sejumlah " + biayaantar.getText().toString() + "ke Rek. 324424234 a.n Kalif Ardy sebelum pukul 16.00");
         call.enqueue(new Callback<ResponseBody>() {
             @Override
             public void onResponse(Call<ResponseBody> call, Response<ResponseBody> response) {
                 progress.dismiss();
-                if (response.isSuccessful()){
+                if (response.isSuccessful()) {
                     JSONObject json = null;
                     try {
                         json = new JSONObject(response.body().string());
                         String message = json.getString("message");
-                        Toast.makeText(DetailObatActivity.this, ""+message, Toast.LENGTH_SHORT).show();
+                        Toast.makeText(DetailObatActivity.this, "" + message, Toast.LENGTH_SHORT).show();
 
                     } catch (JSONException e) {
                         e.printStackTrace();
@@ -288,68 +210,47 @@ public class DetailObatActivity extends AppCompatActivity {
         hargaobat.setText("" + Integer.parseInt(dataHarga) * quantity);
         perkiraanharga.setText("" + Integer.parseInt(dataHarga) * quantity);
         biayaantar.setText("" + (5000));
-        totalharga.setText("" + ((Integer.parseInt(dataHarga) * quantity)+5000));
-        bayartunai.setText("" + ((Integer.parseInt(dataHarga) * quantity)+5000));
+        totalharga.setText("" + ((Integer.parseInt(dataHarga) * quantity) + 5000));
+        bayartunai.setText("" + ((Integer.parseInt(dataHarga) * quantity) + 5000));
 
         display(quantity);
     }
 
+
+    public void tambah(View v) {
+        if (quantity == 100) {
+            Toast.makeText(this, "pesanan maksimal 100", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        quantity = quantity + 1;
+        hargaobat.setText("" + Integer.parseInt(dataHarga) * quantity);
+        perkiraanharga.setText("" + Integer.parseInt(dataHarga) * quantity);
+        biayaantar.setText("" + (5000));
+        totalharga.setText("" + ((Integer.parseInt(dataHarga) * quantity) + 5000));
+        bayartunai.setText("" + ((Integer.parseInt(dataHarga) * quantity) + 5000));
+
+        display(quantity);
     }
 
 
 
+    private void display(int number) {
+        TextView jumlahobat = (TextView) findViewById(R.id.jmlobat);
+        jumlahobat.setText("" + number);
+    }
 
-    private void kirimNotif() {
-
-            }
-
-            public void kurang(View v) {
-                if (quantity == 1) {
-                    Toast.makeText(this, "pesanan minimal 1", Toast.LENGTH_SHORT).show();
-                    return;
-                }
-                quantity = quantity - 1;
-                hargaobat.setText("" + Integer.parseInt(dataHarga) * quantity);
-                perkiraanharga.setText("" + Integer.parseInt(dataHarga) * quantity);
-                biayaantar.setText("" + ((Integer.parseInt(dataHarga) * quantity) + 5000));
-                totalharga.setText("" + ((Integer.parseInt(dataHarga) * quantity) + 5000));
-                bayartunai.setText("" + ((Integer.parseInt(dataHarga) * quantity) + 5000));
-
-                display(quantity);
-            }
-
-            public void tambah(View v) {
-                if (quantity == 100) {
-                    Toast.makeText(this, "pesanan maximal 100", Toast.LENGTH_SHORT).show();
-                    return;
-                }
-                quantity = quantity + 1;
-                hargaobat.setText("" + Integer.parseInt(dataHarga) * quantity);
-                perkiraanharga.setText("" + Integer.parseInt(dataHarga) * quantity);
-                biayaantar.setText("" + ((Integer.parseInt(dataHarga) * quantity) + 5000));
-                totalharga.setText("" + ((Integer.parseInt(dataHarga) * quantity) + 5000));
-                bayartunai.setText("" + ((Integer.parseInt(dataHarga) * quantity) + 5000));
-
-                display(quantity);
-            }
-
-            private void display(int number) {
-                TextView jumlahobat = (TextView) findViewById(R.id.jmlobat);
-                jumlahobat.setText("" + number);
-            }
-
-            private void initView() {
-                btnBeli = (Button) findViewById(R.id.btnPesan);
-                Obat = findViewById(R.id.Obat);
-                HargaObat = findViewById(R.id.HargaObat);
-                Deskripsi = findViewById(R.id.Deskripsi);
-                tambahcatatan = findViewById(R.id.tambahcatatan);
-                jmlobat = findViewById(R.id.jmlobat);
-                perkiraanharga = findViewById(R.id.perkiraanharga);
-                biayaantar = findViewById(R.id.biayaantar);
-                totalharga = findViewById(R.id.totalharga);
-                bayartunai = findViewById(R.id.bayartunai);
-                btnPesan = findViewById(R.id.btnPesan);
-            }
-        }
+    private void initView() {
+        btnBeli = (Button) findViewById(R.id.btnPesan);
+        Obat = findViewById(R.id.Obat);
+        HargaObat = findViewById(R.id.HargaObat);
+        Deskripsi = findViewById(R.id.Deskripsi);
+        tambahcatatan = findViewById(R.id.tambahcatatan);
+        jmlobat = findViewById(R.id.jmlobat);
+        perkiraanharga = findViewById(R.id.perkiraanharga);
+        biayaantar = findViewById(R.id.biayaantar);
+        totalharga = findViewById(R.id.totalharga);
+        bayartunai = findViewById(R.id.bayartunai);
+        btnPesan = findViewById(R.id.btnPesan);
+    }
+}
 
